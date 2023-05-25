64357cc57607be791b2ae295994a10654f69efc2
mohanasreekatam <mohanasreekatam7@gamil.com>
2023-05-25 15:57:12 +0530

    modified main file

--- src/threading/add.java
+++ src/threading/add.java
@@ -1,16 +1,8 @@
 package threading;
 
 public class add {
-    public static void main(String[] args){
-        int a =10;
-           int b =20 ;
-        int c = add(a,b) ;
-        System.out.println(c);
-        System.out.println(c);
 
-    }
-
-public static int add(int a,int b){
+    public static int add(int a,int b){
         return a+b;
 }
 }
